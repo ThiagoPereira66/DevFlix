aca99c396a93ca5bc9dc5f2aa55995b74e4e87de
Thiago Pereira <116083951+ThiagoPereira66@users.noreply.github.com>
2023-10-16 01:46:08 -0300

    Confg rotas

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import GlobalStyle from './style/globalStyle'
 
-import Router from './routes/routes.jsx'
+import Rotas from './routes/routes.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <HashRouter>
-      <Router />
+      <Rotas />
       <GlobalStyle/>
     </HashRouter>
 
